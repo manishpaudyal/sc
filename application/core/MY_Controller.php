96e9dd5c2101352b944a713a80bd963a10dfcf3d
manishpaudyal <manishpaudyal@yahoo.com>
2015-09-04 14:00:25 +0545

    Login Check
    Check if user has logged in or not .

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -7,5 +7,20 @@ class MY_Controller extends MX_Controller{
 		parent::__construct();
 		$this->data['errors']=array();
 		$this->data['site_name']=config_item('site_name');
+		$this->load->library(array('ion_auth','form_validation'));
+
+
+		//Login Check
+		$exception_uris= array(
+			'auth/login',
+			'auth/logout'
+		); 
+		if(in_array(uri_string(), $exception_uris)==FALSE){
+			if (!$this->ion_auth->logged_in())
+			{
+				// redirect them to the login page
+				redirect('auth/login', 'refresh');
+			}
+		}
 	}
 }
